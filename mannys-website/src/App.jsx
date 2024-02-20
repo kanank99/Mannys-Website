2f3d17deef0c710ba8b8c9296fffda4cd3924f68
Kanan Kontorovsky <kanankontorovsky@hotmail.com>
2024-02-20 13:48:16 -0500

    designed MenuItem

--- mannys-website/src/App.jsx
+++ mannys-website/src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
+import MenuItem from "./components/MenuItem";
+import lasagna from "./assets/menu/lasagna.jpg";
 
 function App() {
   return (
-    <div className="">
+    <div className="h-[1000px]">
       <Header />
-      <div className="h-[1000px]"></div>
+      <div className="flex justify-center items-center mt-[200px]">
+        <MenuItem
+          itemTitle="Homemade Lasagna"
+          backgroundImage={lasagna}
+          description="your choice of veggi or meat lasagna, towering layers of noodles, a ruch homemade meat/or marinara sauce, mozzarella, and parmesan cheese"
+          price="13.99"
+        />
+      </div>
     </div>
   );
 }
---
+++ mannys-website/src/components/MenuItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function MenuItem(props) {
+  return (
+    <div className="w-[600px] h-[200px] rounded-lg overflow-hidden border border-gray-400 shadow-lg">
+      <div className="flex items-center">
+        <div className="grow-[2] flex-col p-4">
+          <h1 className="text-xl font-bold">{props.itemTitle}</h1>
+          <p className="text-sm w-[90%] my-2 text-[#565462] font-serif">
+            {props.description}
+          </p>
+          <p className="text-xl font-bold text-[#138AB6]">${props.price}</p>
+        </div>
+
+        <img
+          className="
+            w-[200px] h-[200px] object-cover grow-[1]
+          "
+          src={props.backgroundImage}
+          alt={props.itemTitle}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MenuItem;
