82919a63c37e30b69c8a28589f641a2786138e57
Kanan Kontorovsky <kanankontorovsky@hotmail.com>
2024-02-21 21:20:13 -0500

    Update menu intro responsiveness and add mobile background image

--- mannys-website/src/components/Menu.jsx
+++ mannys-website/src/components/Menu.jsx
@@ -54,19 +54,63 @@ function Menu() {
 
   return (
     <>
-      <div className="max-w-[1200px] mx-auto pb-[40px] pt-[60px]">
-        <div className="flex justify-between items-center mx-[30px]">
-          <div className="max-w-[400px]">
-            <div className="mb-[20px]">
-              <h1 className="text-[44px] font-bold leading-[50px]">
-                Ordering From <span className="text-[#004f71]">Manny's</span>
+      <div className="hidden md:block">
+        <div className="max-w-[1200px] mx-auto pb-[40px] pt-[60px]">
+          <div className="flex justify-between items-center mx-[30px]">
+            <div className="max-w-[400px]">
+              <div className="mb-[20px]">
+                <h1 className="text-[44px] font-bold leading-[50px]">
+                  Ordering From <span className="text-[#004f71]">Manny's</span>
+                </h1>
+                <p className="text-sm text-[#565462] font-serif mt-[20px]">
+                  2520 Boiling Springs Rd., Boiling Springs, S.C. 29316
+                </p>
+              </div>
+              <a
+                class="mt-[40px] inline-flex justify-center items-center gap-x-3 text-center w-[200px] bg-gradient-to-tl from-[#004F71] to-[#138AB5] hover:brightness-90 text-white font-semibold py-3 px-6 rounded-lg transition-all duration-300"
+                href="#"
+              >
+                Take Out
+                <svg
+                  class="flex-shrink-0 size-4"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                >
+                  <path
+                    d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="max-h-[362px] overflow-hidden rounded-xl">
+              <img
+                src={introImg}
+                alt="introImg"
+                className="bg-cover w-[362px] -translate-y-[160px]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="md:hidden">
+        <div className="menuIntroBgImgMobile w-full h-[500px] flex items-center justify-center text-center text-white">
+          <div className="w-full">
+            <div className="mb-[30px]">
+              <h1 className="text-[44px] font-bold leading-[50px] word-stroke-2">
+                Ordering From{" "}
+                <span className="text-[#138AB5] word-stroke-1">Manny's</span>
               </h1>
-              <p className="text-sm text-[#565462] font-serif mt-[20px]">
+              <p className="text-sm text-[white] font-serif mt-[10px]">
                 2520 Boiling Springs Rd., Boiling Springs, S.C. 29316
               </p>
             </div>
             <a
-              class="mt-[40px] inline-flex justify-center items-center gap-x-3 text-center w-[200px] bg-gradient-to-tl from-[#004F71] to-[#138AB5] hover:from-[#138AB5] hover:to-[#004F71] text-white font-semibold py-3 px-6 rounded-lg transition-all duration-300"
+              class="inline-flex justify-center items-center gap-x-3 text-center w-[200px] bg-gradient-to-tl from-[#004F71] to-[#138AB5] hover:brightness-90 text-white font-semibold py-3 px-6 rounded-lg transition-all duration-300"
               href="#"
             >
               Take Out
@@ -86,25 +130,18 @@ function Menu() {
               </svg>
             </a>
           </div>
-          <div className="max-h-[362px] overflow-hidden rounded-xl">
-            <img
-              src={introImg}
-              alt="introImg"
-              className="bg-cover w-[362px] -translate-y-[160px]"
-            />
-          </div>
         </div>
       </div>
       <div className="w-full h-[60px] sticky top-0 border-t border-b border-gray-400 bg-white z-20">
-        <div className="max-w-[1200px] h-full mx-auto flex justify-between items-center px-4 relative overflow-hidden select-none">
+        <div className="max-w-[1200px] h-full mx-auto flex justify-between items-center px-4 relative overflow-scroll no-scrollbar md:overflow-hidden select-none">
           {showRightFade && (
-            <div className="absolute left-0 top-0 w-full h-full menuNavFadeRight z-10 pointer-events-none"></div>
+            <div className="hidden md:block absolute left-0 top-0 w-full h-full menuNavFadeRight z-10 pointer-events-none"></div>
           )}
           {showLeftFade && (
-            <div className="absolute right-0 top-0 w-full h-full menuNavFadeLeft z-10 pointer-events-none"></div>
+            <div className="hidden md:block absolute right-0 top-0 w-full h-full menuNavFadeLeft z-10 pointer-events-none"></div>
           )}
           {showArrow && (
-            <div className="absolute right-[20px] top-[15px] z-10">
+            <div className="hidden md:block absolute right-[20px] top-[15px] z-10">
               <img
                 src={arrow}
                 alt="arrow"
@@ -114,7 +151,7 @@ function Menu() {
             </div>
           )}
           {showArrowBack && (
-            <div className="absolute left-[20px] top-[15px] z-10">
+            <div className="hidden md:block absolute left-[20px] top-[15px] z-10">
               <img
                 src={arrow}
                 alt="arrow"
@@ -149,7 +186,7 @@ function Menu() {
             ].map((category) =>
               selectedCategory === category ? (
                 <p
-                  className={`text-lg font-semibold px-[40px] w-full h-[30px] flex ${
+                  className={`text-md md:text-lg font-semibold px-[20px] md:px-[40px] w-full h-[30px] flex ${
                     selectedCategory === "Specials"
                       ? null
                       : "border-l border-[#aaa]"
@@ -159,7 +196,7 @@ function Menu() {
                 </p>
               ) : (
                 <p
-                  className={`text-lg font-semibold px-[40px] ${
+                  className={`text-md md:text-lg font-semibold px-[20px] md:px-[40px] ${
                     category === "Specials" ? null : "border-l border-[#aaa]"
                   }  w-full h-[30px] flex items-center text-nowrap cursor-pointer`}
                   onClick={() => setSelectedCategory(category)}
